30cdafee49a7f32aaf1af167e1725ab75fa3ca8e
Aninda Betal <michaelshroud01@gmail.com>
2024-03-08 03:01:17 +0530

    Function and Doc string

---
+++ 23-function.py
@@ -0,0 +1,16 @@
+def printHello():
+    return "Hello"
+
+def add():
+    """This is the function that calculate 2 numbers addition"""
+    a = int(input("Enter a number: \n"))
+    b = int(input("Enter anothe number: \n"))
+    return f"The resule is {a+b}"
+
+# print(add())
+print(printHello())
+
+v = add()
+print(v)
+
+print(add. __doc__)     # This is example of doc string
