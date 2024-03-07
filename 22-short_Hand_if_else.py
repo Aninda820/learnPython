0eff913ba08a68fbfce32f42bb5dc94610268c2e
Aninda Betal <michaelshroud01@gmail.com>
2024-03-08 03:09:54 +0530

    Short hand if else

---
+++ 22-short_Hand_if_else.py
@@ -0,0 +1,5 @@
+a = int(input("Enter a number as input: "))
+b = int(input("Enter another numebr as input: "))
+# if a>b: print("A B say bada hay bhi")
+
+print("A is gratter then B") if a>b else print("B is gratter then A")
