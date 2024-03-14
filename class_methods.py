c795dca94d1a977ac6e5b980818af5b316f74450
Aninda Betal <michaelshroud01@gmail.com>
2024-03-15 02:46:20 +0530

    Class Methods in python

---
+++ class_methods.py
@@ -0,0 +1,27 @@
+class Employee:
+    no_of_leaves = 8
+    def __init__(self, name, salary, role):
+        self.name = name
+        self.salary = salary
+        self.role = role
+
+    def print_details(self):
+        return f"The name {self.name} salary {self.salary} and role is {self.role}"
+
+
+    @classmethod
+    def change_leaves(cls, newLeaves):
+        cls.no_of_leaves = newLeaves
+
+
+
+ram = Employee("Ram", "40k", "GOD")
+krishna = Employee("Krishna", "66k", "GOD")
+
+ram.change_leaves(34)
+print(ram.no_of_leaves)
+print(Employee.no_of_leaves)
+
+print(ram.print_details())
+print(krishna.print_details())
+
