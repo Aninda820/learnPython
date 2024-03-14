b0c601930f1955ae03d9fc0ee23a50ac4a1d1ced
Aninda Betal <michaelshroud01@gmail.com>
2024-03-14 23:50:59 +0530

    How to create Class and Object in python

---
+++ class_and_object.py
@@ -0,0 +1,15 @@
+class Student:
+    pass
+
+
+harry = Student()
+larry = Student()
+harry.name = "Harry"
+harry.section = "A"
+harry.subject = ['English', 'Math', 'Computer']
+print(harry.section)
+print(harry.name, harry.section, harry.subject)
+print(harry.subject)
+
+
+# print(harry, larry)   ## location in Memory 
