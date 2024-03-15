24c1995dec13297932235457c398222965d2d0a0
Aninda Betal <michaelshroud01@gmail.com>
2024-03-15 21:07:42 +0530

    Prime Number program

--- prime_number.py
+++ prime_number.py
@@ -1,19 +1,47 @@
+# def is_prime(num):
+#     if num <= 1:
+#         return False
+#     for i in range(2, int(num**0.5) + 1):
+#         if num % i == 0:
+#             return False
+#     return True
+
+# def generate_primes(n):
+#     primes = []
+#     num = 2
+#     while len(primes) < n:
+#         if is_prime(num):
+#             primes.append(num)
+#         num += 1
+#     return primes
+
+# print(generate_primes(20))
+
+
+
+
+# Another way
 def is_prime(num):
-    if num <= 1:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+  """
+  This function checks if a number is prime.
 
-def generate_primes(n):
-    primes = []
-    num = 2
-    while len(primes) < n:
-        if is_prime(num):
-            primes.append(num)
-        num += 1
-    return primes
+  Args:
+    num: The number to check.
 
-print(generate_primes(20))
+  Returns:
+    True if the number is prime, False otherwise.
+  """
+  if num <= 1:
+    return False
+  for i in range(2, int(num**0.5) + 1):
+    if num % i == 0:
+      return False
+  return True
 
+count = 0
+num = 2
+while count < 20:
+  if is_prime(num):
+    print(num)
+    count += 1
+  num += 1
