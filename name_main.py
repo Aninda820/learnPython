cdd4777e39d1745f37865db9b5ac3988decffdff
Aninda Betal <michaelshroud01@gmail.com>
2024-03-16 03:03:46 +0530

    if __name__ == '__main__'

---
+++ name_main.py
@@ -0,0 +1,12 @@
+def print_hello(string):
+    return f"hello {string}"
+
+def sum(a, b):
+    return a+b
+
+def squer(a):
+    return a**2
+
+if __name__ == "__main__":
+    print(print_hello("Aninda"))
+    print(sum(4, 7))
---
+++ name_main2.py
@@ -0,0 +1,5 @@
+import name_main
+print(name_main.sum(3, 5))
+print(name_main.print_hello("Itachi"))
+
+print(name_main.squer(3))
