6806ba698a8c21a925aea1b42fda4eb0d239015f
Aninda Betal <michaelshroud01@gmail.com>
2024-03-15 23:58:38 +0530

    Class Methods as Alternative Constructors

---
+++ alternative_cons.py
@@ -0,0 +1,35 @@
+class Employee:
+    no_of_leaves = 8
+    def __init__(self, name, salary, role):
+        self.name = name
+        self.salary = salary
+        self.role = role
+
+    def print_details(self):
+        return f"The name {self.name} salary {self.salary} and role is {self.role}"
+
+
+    @classmethod
+    def change_leaves(cls, newLeaves):
+        cls.no_of_leaves = newLeaves
+
+    @classmethod
+    def from_str(cls, string):
+        # params = string.split("-")
+        # return cls(params[0], params[1], params[2])
+        return cls(*string.split("-"))          # one liner code (example of args and kwargs)
+
+
+ram = Employee("Ram", "40k", "GOD")
+krishna = Employee("Krishna", "66k", "GOD")
+karan = Employee.from_str("Karan-480-Student")
+
+# ram.change_leaves(34)
+# print(ram.no_of_leaves)
+# print(Employee.no_of_leaves)
+
+print(ram.print_details())
+print(krishna.print_details())
+print(karan.print_details())
+print(karan.name)
+
