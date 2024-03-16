474c3c37bb6184eb77af8edc1c138d8bd1f144bb
Aninda Betal <michaelshroud01@gmail.com>
2024-03-16 18:19:15 +0530

    File IO About

---
+++ file_IO.py
@@ -0,0 +1,8 @@
+# "r" - Open file for reading       - Default mode
+# "w" - Open a file for writing
+# "x" - Creates file if-not exists
+# "a" - Add more content to a file
+# "t" - text mode           - Default mode 
+# "b" - Binary mode
+# "+" - Read and Write
+
