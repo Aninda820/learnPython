8b29e93de794703927c829e864621f8865113eeb
Aninda Betal <michaelshroud01@gmail.com>
2024-03-16 05:32:29 +0530

    Dictionary Methods in python

---
+++ dic_method.py
@@ -0,0 +1,29 @@
+ep1 = {
+    123: "45%",
+    445: "76%",
+    324: "94%",
+    745: "55%"
+}
+ep2 = {
+    333: "67%",
+    843: "48%",
+}
+
+# ep1.clear()
+# print(ep1)
+
+# ep1.update(ep2)
+# print(ep1)
+
+
+# ep1.pop(123)
+# print(ep1)
+
+# ep1.popitem()     # it delete items from last
+# print(ep1)
+
+
+# del ep1           # use del keyword for delete
+
+
+print(ep1)
